03e5bfb130e929b53273ac5168312edc7f09b03f
majufigueiredo <143432089+majufigueiredo@users.noreply.github.com>
2024-06-11 15:14:31 -0300

    Update ProjetoProgA2.py

--- ProjetoProgA2.py
+++ ProjetoProgA2.py
@@ -1,21 +1,34 @@
+import streamlit as st
+import requests
+# Defina sua chave API
+api_key = "88193101454b15bf710f79d9106882aa"
+# Função para adicionar uma imagem de fundo
+def add_bg_from_url():
+    st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background-image: url("https://raw.githubusercontent.com/majufigueiredo/ProjetoProgA2/upload/main/nuvem");
+             background-image: url("https://raw.githubusercontent.com/majufigueiredo/ProjetoProgA2/main/foto.fundo.nuvem.jpg");
+             background-size: cover;
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
 # Adiciona a imagem de fundo
 add_bg_from_url()
-
 # Título da aplicação
 st.title("Consulta de Clima")
-
 # Campo de entrada para o nome da cidade
 cidade = st.text_input("Digite o nome da cidade:")
-
 # Se o usuário inseriu uma cidade
 if cidade:
     # Define a URL da API com os parâmetros necessários
     url = f"http://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={api_key}&units=metric&lang=pt_br"
-
     # Faz a solicitação para a API
     response = requests.get(url)
     dados_clima = response.json()
-
     # Verifica se a cidade foi encontrada
     if response.status_code == 200:
         # Extrai as informações importantes do JSON retornado
@@ -25,7 +38,6 @@ if cidade:
         sensacao_termica = dados_clima['main']['feels_like']
         umidade = dados_clima['main']['humidity']
         descricao = dados_clima['weather'][0]['description']
-
         # Exibe as informações
         st.subheader(f"Clima em {cidade.capitalize()}:")
         st.write(f"**Descrição:** {descricao.capitalize()}")
