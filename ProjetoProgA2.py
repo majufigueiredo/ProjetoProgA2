3b83c50b4988a8d9477a667be1f2c274f4fe6b0b
majufigueiredo <143432089+majufigueiredo@users.noreply.github.com>
2024-06-11 15:20:08 -0300

    Update ProjetoProgA2.py

--- ProjetoProgA2.py
+++ ProjetoProgA2.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import requests
+
 # Defina sua chave API
 api_key = "88193101454b15bf710f79d9106882aa"
+
 # Função para adicionar uma imagem de fundo
 def add_bg_from_url():
     st.markdown(
@@ -16,19 +18,25 @@ def add_bg_from_url():
          """,
          unsafe_allow_html=True
      )
+
 # Adiciona a imagem de fundo
 add_bg_from_url()
+
 # Título da aplicação
 st.title("Consulta de Clima")
+
 # Campo de entrada para o nome da cidade
 cidade = st.text_input("Digite o nome da cidade:")
+
 # Se o usuário inseriu uma cidade
 if cidade:
     # Define a URL da API com os parâmetros necessários
     url = f"http://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={api_key}&units=metric&lang=pt_br"
+
     # Faz a solicitação para a API
     response = requests.get(url)
     dados_clima = response.json()
+
     # Verifica se a cidade foi encontrada
     if response.status_code == 200:
         # Extrai as informações importantes do JSON retornado
@@ -38,13 +46,18 @@ if cidade:
         sensacao_termica = dados_clima['main']['feels_like']
         umidade = dados_clima['main']['humidity']
         descricao = dados_clima['weather'][0]['description']
+
         # Exibe as informações
         st.subheader(f"Clima em {cidade.capitalize()}:")
         st.write(f"**Descrição:** {descricao.capitalize()}")
-        st.write(f"**Temperatura Atual:** {temperatura_atual}°C")
-        st.write(f"**Temperatura Mínima:** {temperatura_minima}°C")
-        st.write(f"**Temperatura Máxima:** {temperatura_maxima}°C")
-        st.write(f"**Sensação Térmica:** {sensacao_termica}°C")
+        
+        # Exibe as métricas
+        col1, col2, col3, col4 = st.columns(4)
+        col1.metric("Temperatura Atual", f"{temperatura_atual}°C")
+        col2.metric("Temperatura Mínima", f"{temperatura_minima}°C")
+        col3.metric("Sensação Térmica", f"{sensacao_termica}°C")
+        col4.metric("Temperatura Máxima", f"{temperatura_maxima}°C")
+        
         st.write(f"**Umidade:** {umidade}%")
     else:
         st.error(f"Não foi possível encontrar o clima para a cidade '{cidade}'. Verifique o nome e tente novamente.")
